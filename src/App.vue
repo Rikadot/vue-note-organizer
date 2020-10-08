2c02cbed64f7ddf635c36159abab6518c5c39129
Sarah <sarah.poloczek@t-online.de>
2020-10-09 01:23:26 +0200

    recreating project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <c-theme-provider>
+    <c-reset />
+    <router-view />
+  </c-theme-provider>
 </template>
 
 <script>
-import Home from './components/Home.vue'
-
 export default {
-  name: 'App',
-  components: {
-    View
-  }
-}
+  name: "App"
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
