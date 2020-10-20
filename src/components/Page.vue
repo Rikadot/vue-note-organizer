f70d2d4c1c1a1b3cacd1fe1418c9a4369aa99b5e
Sarah <sarah.poloczek@t-online.de>
2020-10-20 02:35:58 +0200

    Creating the home page

---
+++ src/components/Page.vue
@@ -0,0 +1,48 @@
+<template>
+<div class = "general">
+    <div class = "layoutPageOuter">        
+        <div class = "navbar">
+            <p>
+                test
+            </p>   
+        </div>
+        <div class = "layoutPageInner">
+            <header>
+                Header
+            </header>
+            <main>
+               here goes the page content 
+            </main>       
+        </div>    
+    </div>
+</div>    
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default ({
+  name: "Page",
+});
+</script>
+
+<style>
+    .general{
+        font: 1.5em sans-serif;
+        color: white;
+    }
+    .layoutPageOuter{
+        display: grid;
+        grid-template-columns: minmax(40px, 10%) 1fr;
+    }
+
+    .layoutPageInner {
+        display: grid;
+        grid-template-rows: minmax(40, 5%) 1fr;
+        background-color: darkblue;
+        place-items: center;
+    }
+
+    .navbar{
+        background-color: darkgreen;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <Page />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Page from "@/components/Page.vue";
 
-export default defineComponent({
+export default ({
   name: "Home",
   components: {
-    HelloWorld
+    Page
   }
 });
 </script>
