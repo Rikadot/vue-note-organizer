13278ab88fc7ba0ba912b1ebbafc628df0f820ad
Sarah <sarah.poloczek@t-online.de>
2020-11-30 13:39:04 +0100

    save current work

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,20 @@
   --highlight: #7182c7;
   --althighlight: #516fe6;
 }
+
+#button-round{
+  color:var(--textcolor);
+  background-color: var(--highlight);
+  border-radius: 50%;
+  border-style: none;
+  text-align: center;
+  font: 1.2em sans-serif;
+  cursor: pointer;
+  padding: 15px;
+  margin-top: 10px;
+  width: 2em;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
 </style>
